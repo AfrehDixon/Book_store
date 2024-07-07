b4b793d0e40640489ce3ba3ad5e8f3f6ad0f7c59
Dixon-Afreh <dixonafreh20@gmail.com>
2024-07-07 23:55:34 +0000

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Route, Routes } from "react-router";
-import "./App.css";
-import Home from "../src/component/Home/Home";
-import Login from "./component/Login/Login";
+// import { Route, Routes } from "react-router";
+// import "./App.css";
+// import Home from "../src/component/Home/Home";
+// import Login from "./component/Login/Login";
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<h1>Contact</h1>} />
-      </Routes>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/contact" element={<h1>Contact</h1>} />
+//       </Routes>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
