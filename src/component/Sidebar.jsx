61fd8c9f50b4aa5d15807d140289ae183e71a748
Dixon-Afreh <dixonafreh20@gmail.com>
2024-07-07 23:56:51 +0000

    new

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,39 +1,39 @@
-// import { Flex, Menu } from "antd";
-// import { BiBook } from "react-icons/bi";
+import { Flex, Menu } from "antd";
+import { BiBook } from "react-icons/bi";
 
-// import { TiUserOutline } from "react-icons/ti";
+import { TiUserOutline } from "react-icons/ti";
 
-// export default function Sidebar() {
-//   return (
-//     <div>
-//       <Flex>
-//         <div>
-//           <BiBook size={40} />
-//         </div>
-//       </Flex>
+export default function Sidebar() {
+  return (
+    <div>
+      <Flex>
+        <div>
+          <BiBook size={40} />
+        </div>
+      </Flex>
 
-//       <Menu
-//         mode="inline"
-//         defaultSelectedKeys={["1"]}
-//         className="menu-bar"
-//         items={[
-//           {
-//             key: "1",
-//             icon: <TiUserOutline />,
-//             label: "Dashboard",
-//           },
-//           {
-//             key: "2",
-//             icon: <TiUserOutline />,
-//             label: "Add Book",
-//           },
-//           {
-//             key: "3",
-//             icon: <TiUserOutline />,
-//             label: "Dashboard",
-//           },
-//         ]}
-//       />
-//     </div>
-//   );
-// }
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+        className="menu-bar"
+        items={[
+          {
+            key: "1",
+            icon: <TiUserOutline />,
+            label: "Dashboard",
+          },
+          {
+            key: "2",
+            icon: <TiUserOutline />,
+            label: "Add Book",
+          },
+          {
+            key: "3",
+            icon: <TiUserOutline />,
+            label: "Dashboard",
+          },
+        ]}
+      />
+    </div>
+  );
+}
